d1b25176cc7a4f87011534c33b6a73c77973ee37
hrishabhsharma <sharma17h@gmail.com>
2023-09-14 00:22:45 +0530

    adding lazy loading to routing

--- src/blog_project/Component/RouteComp.js
+++ src/blog_project/Component/RouteComp.js
@@ -1,26 +1,43 @@
-import React from 'react'
+import React, { Suspense, lazy } from 'react'
 import { Routes , Route, Outlet } from 'react-router-dom'
-import Home from './Home'
-import Pages from './Pages'
-import CardDetail from './CardDetail'
-import ErrorPage from './ErrorPage'
 import HeadNavbar from './HeadNavbar'
 import PageFooter from './PageFooter'
+import ErrorPage from './ErrorPage'
+// import Home from './Home'
+// import Pages from './Pages'
+// import CardDetail from './CardDetail'
+const Home = lazy(()=>import('./Home'))
+const Pages = lazy(()=>import('./Pages'))
+const CardDetail = lazy(()=>import('./CardDetail'))
 
 const RouteComp = () => {
   return (
     <>
     <Routes>
         <Route path="/" element={<><HeadNavbar/><Outlet/><PageFooter/></>}>
-            <Route  index              element={<Home/>}/>
-            <Route  path='/bollywood'  element={<Pages category={'bollywood'}/>}/>
-            <Route  path='/technology' element={<Pages category={'technology'}/>}/>
-            <Route  path='/hollywood'  element={<Pages category={'hollywood'}/>}/>
-            <Route  path='/fitness'    element={<Pages category={'fitness'}/>}/>
-            <Route  path='/food'       element={<Pages category={'food'}/>}/>
+            <Route  index              element={<Suspense fallback={<div><h1>Loading ...</h1></div>}>
+                                                 <Home/>
+                                               </Suspense>}/>
+            <Route  path='/bollywood'  element={<Suspense fallback={<div><h1>Loading ...</h1></div>}>
+                                                 <Pages category={'bollywood'}/>
+                                               </Suspense>}/>
+            <Route  path='/technology' element={<Suspense fallback={<div><h1>Loading ...</h1></div>}>
+                                                 <Pages category={'technology'}/>
+                                               </Suspense>}/>
+            <Route  path='/hollywood'  element={<Suspense fallback={<div><h1>Loading ...</h1></div>}>
+                                                 <Pages category={'hollywood'}/>
+                                               </Suspense>}/>
+            <Route  path='/fitness'    element={<Suspense fallback={<div><h1>Loading ...</h1></div>}>
+                                                 <Pages category={'fitness'}/>
+                                               </Suspense>}/>
+            <Route  path='/food'       element={<Suspense fallback={<div><h1>Loading ...</h1></div>}>
+                                                  <Pages category={'food'}/>
+                                                </Suspense>}/>
         </Route>
-        <Route  path='/:genre/:id' element={<CardDetail/>}/>
-        <Route  path='/*'          element={<ErrorPage/>}/>
+        <Route  path='/:genre/:id'     element={<Suspense fallback={<div><h1>Loading ...</h1></div>}>
+                                                   <CardDetail/>
+                                               </Suspense>}/>
+        <Route  path='/*'              element={<ErrorPage/>}/>
     </Routes>
     </>
   )
